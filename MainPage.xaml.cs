2be634a38d70ebb8aadff2d8d27eb5a73925565f
nic00la1 <nic00la.kaleta@gmail.com>
2024-01-24 08:50:40 +0100

    Searchicon works for Android ✅

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -19,9 +19,13 @@ namespace FairyTalesDemo
             {
 #if ANDROID
              var context = handler.PlatformView.Context;
-             var searchIconId = context.Resources.GetIdentifier("search_mag_icon", "id", context);
-             if (searchIconId != 0) {
+             var searchIconId = context.Resources.GetIdentifier("search_mag_icon", "id", context.PackageName);
+             if (searchIconId != 0) 
+             {
+                var searchIcon = handler.PlatformView.FindViewById<Android.Widget.ImageView>(searchIconId);
+                searchIcon.SetColorFilter(Android.Graphics.Color.Rgb(172, 157, 185), Android.Graphics.PorterDuff.Mode.SrcIn);
              }
+#endif
             });
         }
     }
