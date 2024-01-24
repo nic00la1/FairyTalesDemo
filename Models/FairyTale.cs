b918e0a15139c4f729df848834bb8b9ff53af9a1
nic00la1 <nic00la.kaleta@gmail.com>
2024-01-24 08:54:32 +0100

    Model done ✅

---
+++ Models/FairyTale.cs
@@ -0,0 +1,9 @@
+﻿namespace FairyTalesDemo.Models
+{
+    internal class FairyTale
+    {
+        public string Name { get; set; }
+        public TimeSpan ReadTime { get; set; }
+        public string Image { get; set; }
+    }
+}
